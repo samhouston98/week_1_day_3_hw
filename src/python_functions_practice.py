198dfa2594413b2911741736a807d910117a2cf3
sam <sam@beths-MacBook-Pro.local>
2023-03-22 15:36:39 +0000

    complete_hw

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,2 +1,48 @@
 def return_10():
-    return_10
+    return 10
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def length_of_string(string):
+    return len(string)
+
+
+def join_string(string1, string2):
+    return string1 + string2
+
+def add_string_as_number(string1, string2):
+    return int(string1) + int(string2) 
+
+def number_to_full_month_name(num1):
+    if num1 == 1:
+        return "January"
+
+    if num1 == 3:
+        return "March"
+    
+    if num1 == 9:
+        return "September"
+    
+
+def number_to_short_month_name(num1):
+    if num1 == 1:
+        return "Jan"
+    
+    if num1 == 4:
+        return "Apr"
+    
+    if num1 == 10:
+        return "Oct"
+
+
+
